e9d008b9900a4e66a029a664853300ac030e23a2
Felipe-alv-es <brago.sama@gmail.com>
2023-04-11 00:12:22 -0300

    API connected with home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,20 +1,26 @@
 import Head from "next/head";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import hygraphServiceTypes from "../src/interfaces/hygraphService.types";
 
 export default function Home() {
+  const [hygraphData, getHygraphData] = useState<hygraphServiceTypes[]>([]);
+
   useEffect(() => {
     axios
       .get(
-        "https://api-sa-east-1.hygraph.com/v2/clg4k0jbw25dl01tdghh81uic/master"
+        "https://api-sa-east-1.hygraph.com/v2/clg4k0jbw25dl01tdghh81uic/master",
+        { params: { query: `query getBdData { dataBase1S { id data1 } }` } }
       )
       .then((response) => {
-        console.log(response);
+        getHygraphData(response.data.data.dataBase1S);
       })
       .catch((error) => {
         console.log(error);
       });
   });
+
+  console.log(hygraphData);
 
   return (
     <div
@@ -29,6 +35,12 @@ export default function Home() {
         <title style={{}}>NextJS with Hygraph</title>
       </Head>
       <h1> NextJS with Hygraph </h1>
+      {hygraphData.map((itens) => (
+        <>
+          <li key={itens.id}>{itens.id}</li>
+          <li>{itens.data1}</li>
+        </>
+      ))}
     </div>
   );
 }
--- vscode.sh
+++ vscode.sh
@@ -1,5 +1,2 @@
 code .
-<<<<<<< HEAD
 exit
-=======
->>>>>>> f58381e139a94f17ae189d56eb8fecade8b0ed82
